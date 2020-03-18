16553eaa84bc22d89c01dc903585e96a538fb718
Alberto Aragon <ronin8503@gmail.com>
2020-03-18 16:12:10 -0500

    adding seeds for redirects belonging to links

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
 20.times do
-  url = Faker::Internet.url
-  shortened = Faker::TvShows::FamilyGuy.character
+  url = Faker::Internet.unique.url
+  shortened = Faker::Number.unique.number
 
-  Link.create( url: url, shortened: shortened)
+  link = Link.create( url: url, shortened: shortened )
+
+  redirects = []
+  3.times do
+    redirects.push(Redirect.new( time: Time.now, location: Faker::Address.city ))
+  end
+
+  redirects.each do |redirect|
+    redirect.link = link
+    redirect.save
+  end
 end

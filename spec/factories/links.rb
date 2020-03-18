fb29d3313c6a5b52a41697ef8152e349fe8a3e84
Alberto Aragon <ronin8503@gmail.com>
2020-03-18 14:48:17 -0500

    make links unique and case-insensitive

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :link do
-    url { Faker::Internet.url }
-    shortened { Faker::TvShows::FamilyGuy.character }
+    url { Faker::Internet.unique.url }
+    shortened { Faker::Name.unique.name }
   end
 end

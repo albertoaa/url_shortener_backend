9b60e14b531837c8e451b4c4505b59b11d566d85
Alberto Aragon <ronin8503@gmail.com>
2020-03-18 12:40:57 -0500

    passing rspec for links

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :link do
     url { Faker::Internet.url }
-    shortened { Faker::FamilyGuy.character }
+    shortened { Faker::TvShows::FamilyGuy.character }
   end
 end
--- spec/requests/link_spec.rb
+++ spec/requests/link_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'Links API', type: :request do
   let!(:links) { create_list(:link, 10) }
   let(:link_id) { link.first.id }
 
-  describe 'GET /api/links' do
-    before { get '/api/links' }
+  describe 'all' do
+    before { get '/api/v1/links' }
 
     it 'returns all the links' do
-      expect(json).not_to be_empty
-      expect(json.size).to eq(10)
+      expect(links).not_to be_empty
+      expect(links.size).to eq(10)
     end
 
     it 'returns status code 200' do

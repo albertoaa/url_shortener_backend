4a053cf45ee65598b07f02552002b15e190e3ddc
Alberto Aragon <ronin8503@gmail.com>
2020-03-18 16:49:43 -0500

    fix rack-attack broken logic

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,9 +1,15 @@
 class Rack::Attack
-  #10 request per 10 seconds for a sinlge ip address
+  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
+  # 10 requests per 10 seconds for a single ip
   throttle('req/ip', limit: 10, period: 10) do |req|
     req.ip
   end
-  
+
+  # whitelist localhost
+  Rack::Attack::safelist('allow-localhost') do |req|
+    '127.0.0.1' == req.ip || '::1' == req.ip
+  end
   # response with 429
   self.throttled_response = ->(env) {
     retry_after = (env['rack.attack.match_data'] || {})[:period]
@@ -13,9 +19,4 @@ class Rack::Attack
       [{error: "Throttle limit reached. Retry later."}.to_json]
     ]
   }
-
-  # whitelist localhost
-  safelist('allow-localhost') do |req|
-    '127.0.0.1' == req.ip || '::1' == req.ip
-  end
 end

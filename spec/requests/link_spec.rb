ecfe95dbacd4dcff95b4ba070af2cd22785ce7b3
Alberto Aragon <ronin8503@gmail.com>
2020-03-18 13:05:06 -0500

    adding rspec other links routes

--- spec/requests/link_spec.rb
+++ spec/requests/link_spec.rb
@@ -16,4 +16,37 @@ RSpec.describe 'Links API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'store link' do
+    # valid payload
+    let(:valid_attributes) { {
+      url: Faker::Internet.url,
+      shortened: Faker::TvShows::FamilyGuy.character
+    } }
+
+    context 'when the request is valid' do
+      before { post '/api/v1/links', params: valid_attributes }
+
+      it 'stores a link' do
+        expect(json['url']).to eq(valid_attributes[:url])
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/api/v1/links', params: { url: valid_attributes[:url] } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match("{\"shortened\":[\"can't be blank\"]}")
+      end
+    end
+  end
 end

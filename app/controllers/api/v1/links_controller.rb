377a8d571478890a1774716be4cf8ee3a66c0156
Alberto Aragon <ronin8503@gmail.com>
2020-03-18 23:44:40 -0500

    cleaning up links controller and redoing link_spec

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::LinksController < ApplicationController
-  before_action :set_link, only: [:show, :update, :destroy]
-
   # GET /links
   def index
     @links = Link.all
@@ -8,35 +6,13 @@ class Api::V1::LinksController < ApplicationController
     render json: @links
   end
 
-  # GET /links/1
-  def show
-    render json: @link
-  end
+  def store
+    @link = Link.create!(link_params)
 
-  # POST /links
-  def create
-    @link = Link.new(link_params)
-
-    if @link.save
+    if @link.valid?
       @link.shortened = "#{request.base_url}/#{@link.shortened}"
-      render json: @link, status: :created, location: api_v1_links_get_url(@link)
-    else
-      render json: @link.errors, status: :unprocessable_entity
+      render json: @link, status: :created
     end
-  end
-
-  # PATCH/PUT /links/1
-  def update
-    if @link.update(link_params)
-      render json: @link
-    else
-      render json: @link.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /links/1
-  def destroy
-    @link.destroy
   end
 
   # GET TOTAL OF LINKS /count
@@ -57,11 +33,6 @@ class Api::V1::LinksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_link
-      @link = Link.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def link_params
       params.permit(:url, :shortened)

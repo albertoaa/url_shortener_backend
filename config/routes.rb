fc494f12136a0ce387e05ab852b6bc50aa8a4d76
Alberto Aragon <ronin8503@gmail.com>
2020-07-23 23:43:45 -0500

    moving out redirect controller and updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
       # resources :links
       # resources :redirect
       scope '/links', as: 'links' do
-        post 'create', to: 'links#create', as: 'create'
+        post 'store', to: 'links#store', as: 'store'
         get 'count', to: 'links#count', as: 'count'
         post 'get', to: 'links#get', as: 'get'
         if Rails.env.development? || Rails.env.test?
           get 'index', to: 'links#index', as: 'index'
         end
       end
-      get '/:shortened', to: 'redirect#redirect', as: 'redirect'
     end
   end
+  get '/:shortened', to: 'redirect#redirect', as: 'redirect'
 end
